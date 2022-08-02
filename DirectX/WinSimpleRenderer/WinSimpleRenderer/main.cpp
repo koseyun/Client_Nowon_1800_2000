6176aedea7d3e8885220b070796f896634b9d635
koseyun <101631069+koseyun@users.noreply.github.com>
2022-08-02 19:50:51 +0900

    GameMath
    5일차

---
+++ DirectX/WinSimpleRenderer/WinSimpleRenderer/main.cpp
@@ -0,0 +1,63 @@
+﻿/*
+    
+*/
+
+#pragma once
+#include "framework.h"
+#include <time.h>
+#include <list>
+#include <stack>
+using namespace std;
+
+#pragma comment(lib, "WinApiEngine.lib")
+
+class CAPIEngine: public CAPI_Engine
+{
+public:
+    CAPIEngine() {};
+    virtual ~CAPIEngine() {};
+        
+    virtual void OnCreate() override
+    {
+        CAPI_Engine::OnCreate();
+
+    }
+    virtual void OnDestroy() override
+    {
+        
+        CAPI_Engine::OnDestroy();
+    }
+    virtual void OnUpdate(float tDeltaTime) override
+    {
+        CAPI_Engine::OnUpdate(tDeltaTime);
+
+
+        //Render
+        this->Clear(0.1f, 0.1f, 0.3f);
+        
+        this->Present();
+    }
+
+private:
+    CAPIEngine(const CAPIEngine&) {};
+
+    CAPIEngine& operator=(const CAPIEngine&) {};
+};
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_ LPWSTR    lpCmdLine,
+                      _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    CAPIEngine tEngine;
+    tEngine.Create(hInstance, nCmdShow);
+
+    MSG msg = { 0 };
+
+    msg = tEngine.Run();
+
+    return (int) msg.wParam;
+}
